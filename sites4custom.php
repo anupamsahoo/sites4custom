1cf899712813db6c7823235323536721dd31bcad
Anupam Sahoo <anupammithu@gmail.com>
2018-04-17 00:19:32 +0530

    Added main plugin file which will contain all plugins header with functionality.

---
+++ sites4custom.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: Sites4Contractor Genesis Custom Function V3
+Plugin URI: http://sites4contractors.com/
+Description: Integrates Custom function to overwrite genesis function and user can update logo, slider, mobile number, phone tab for mobile etc.
+Version: 3.0
+Author: Anupam Sahoo
+Author URI: http://sites4contractor.com/
+*/
